2f90e522531ec8e13e5010f2b3840ce45781c50b
JoseTyler101 <JoseTyler101@gmail.com>
2019-06-07 16:10:17 -0400

    edited server .js for port connection

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+
+
 app.get('/', (req,res) => {
   res.send('Hello Again Jose!')
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log("Magic happening on port " + PORT);
 })
