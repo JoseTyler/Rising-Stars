8388d236fe6ed34407721aee31523fc81cb3dc56
JoseTyler101 <JoseTyler101@gmail.com>
2019-06-07 16:18:27 -0400

    edited server for heroku

--- server.js
+++ server.js
@@ -4,9 +4,25 @@ const app = express();
 app.use(express.json());
 
 
-app.get('/', (req,res) => {
-  res.send('Hello Again Jose!')
+
+
+// const routes = require('./routes/index.js')
+
+// app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(`${__dirname}/client/build`))
+
+// app.use('/api/v1', routes)
+// app.get('/', (req,res) => {
+//   res.send('Hello Again Jose!')
+// })
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
 })
+
+
+
+
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
