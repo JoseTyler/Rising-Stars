9bef49fe0cc62a52c3d8616754c1f2c3fd1c1b09
JoseTyler101 <JoseTyler101@gmail.com>
2019-06-07 11:00:46 -0400

    modified server

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.get('/', (req,res) => {
+  res.send('Hello Again Jose!')
+})
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Magic happening on port " + PORT);
+})
