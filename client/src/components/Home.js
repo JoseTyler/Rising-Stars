916bf11e9c87ebdabf49bb32f50852dca454c5b5
JoseTyler101 <JoseTyler101@gmail.com>
2019-06-14 09:42:07 -0400

    started aligning content

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom"
+import styled from "styled-components"
+
+const Header = styled.div`
+    font-size: 30px;
+    position: center;
+    display: flex;
+    justify-content:space-evenly;
+    width: 50vw;
+    color:solid white;
+    font-family: Gobold High;
+    `;
+
+const P = styled.div`
+font-size: 30px;
+position: center;
+display: flex;
+justify-content:space-evenly;
+color:solid white;
+font-family: Gobold High;
+`;
 
 class Home extends Component {
     render() {
         return (
             <div>
-                Im the landing page!
-                <header>
-                    <Link to={'/'} style={{ textDecoration: 'none', color: 'red' }}>Home</Link>
-                    <Link to={'/model'} style={{ textDecoration: 'none', color: 'red' }}>Model Signup</Link>
-                    <Link to={'/models'} style={{ textDecoration: 'none', color: 'red' }}>Models</Link>
-                    <Link to={'/actor'} style={{ textDecoration: 'none', color: 'red' }}>Actor Signup</Link>
-                    <Link to={'/actors'} style={{ textDecoration: 'none', color: 'red' }}>Actors</Link>
-                    <Link to={'/artist'} style={{ textDecoration: 'none', color: 'red' }}>Artist Signup</Link>
-                    <Link to={'/artists'} style={{ textDecoration: 'none', color: 'red' }}>Artists</Link>
-                </header>
+                <Header>
+                    <Link to={'/'} style={{ textDecoration: 'none', color: 'black' }}>Home</Link>
+                    <Link to={'/model'} style={{ textDecoration: 'none', color: 'black' }}>Model Signup</Link>
+                    <Link to={'/models'} style={{ textDecoration: 'none', color: 'black' }}>Models</Link>
+                    <Link to={'/actor'} style={{ textDecoration: 'none', color: 'black' }}>Actor Signup</Link>
+                    <Link to={'/actors'} style={{ textDecoration: 'none', color: 'black' }}>Actors</Link>
+                    <Link to={'/artist'} style={{ textDecoration: 'none', color: 'black' }}>Artist Signup</Link>
+                    <Link to={'/artists'} style={{ textDecoration: 'none', color: 'black' }}>Artists</Link>
+                </Header>
+                <P>Welcome to Rising Stars etc......</P>
             </div>
         );
     }
--- client/src/components/ModelForm.js
+++ client/src/components/ModelForm.js
@@ -3,7 +3,28 @@ import axios from "axios";
 import { Redirect, Link } from "react-router-dom";
 import styled from 'styled-components';
 
+const Header = styled.div`
+    font-size: 30px;
+    position: center;
+    display: flex;
+    justify-content:space-evenly;
+    width: 50vw;
+    color:solid white;
+    font-family: Gobold High;
+    `;
 
+
+const Button = styled.button`
+    height:20px;
+    font-size: 15px;
+    background-color: blue;
+    position: relative;
+    `;
+
+const Input = styled.div`
+    display:flex;
+
+`;
 
 // ModelForm Component
 
@@ -42,34 +63,38 @@ class ModelForm extends Component {
         return (
             <div>
                 <body>
-                    <header>
-                        <Link to={'/'} style={{ textDecoration: 'none', color: 'red' }}>Home</Link>
-                        <Link to={'/model'} style={{ textDecoration: 'none', color: 'red' }}>Model Signup</Link>
-                        <Link to={'/models'} style={{ textDecoration: 'none', color: 'red' }}>Models</Link>
-                        <Link to={'/actor'} style={{ textDecoration: 'none', color: 'red' }}>Actor Signup</Link>
-                        <Link to={'/actors'} style={{ textDecoration: 'none', color: 'red' }}>Actors</Link>
-                        <Link to={'/artist'} style={{ textDecoration: 'none', color: 'red' }}>Artist Signup</Link>
-                        <Link to={'/artists'} style={{ textDecoration: 'none', color: 'red' }}>Artists</Link>
-                    </header>
+                    <Header>
+                        <Link to={'/'} style={{ textDecoration: 'none', color: 'black' }}>Home</Link>
+                        <Link to={'/model'} style={{ textDecoration: 'none', color: 'black' }}>Model Signup</Link>
+                        <Link to={'/models'} style={{ textDecoration: 'none', color: 'black' }}>Models</Link>
+                        <Link to={'/actor'} style={{ textDecoration: 'none', color: 'black' }}>Actor Signup</Link>
+                        <Link to={'/actors'} style={{ textDecoration: 'none', color: 'black' }}>Actors</Link>
+                        <Link to={'/artist'} style={{ textDecoration: 'none', color: 'black' }}>Artist Signup</Link>
+                        <Link to={'/artists'} style={{ textDecoration: 'none', color: 'black' }}>Artists</Link>
+                    </Header>
 
                     <form onSubmit={this.newModel}>
-                        <label htmlFor="name">Name:</label>
-                        <input id="name" name="name" type="text" placeholder="Ex: Jesus Santos" onChange={this.handleChange} />
-                        <label htmlFor="dob">DOB:</label>
-                        <input id="dob" name="dob" placeholder="Ex: 7/12/2010" type="text" onChange={this.handleChange} />
-                        <label htmlFor="ethnicity">Ethnicity:</label>
-                        <input id="ethnicity" name="ethnicity" type="text" placeholder="Ex: White" onChange={this.handleChange} />
-                        <label htmlFor="location">Location:</label>
-                        <input id="location" name="location" type="text" placeholder="Ex: Houston, Tx" onChange={this.handleChange} />
-                        <label htmlFor="photo1">photo1:</label>
-                        <input id="photo1" name="photo1" type="text" placeholder="Ex: photoUrl" onChange={this.handleChange} />
-                        <label htmlFor="photo2">photo2:</label>
-                        <input id="photo2" name="photo2" type="text" placeholder="Ex: photoUrl" onChange={this.handleChange} />
-                        <label htmlFor="photo3">photo3:</label>
-                        <input id="photo3" name="photo3" type="text" placeholder="Ex: photoUrl" onChange={this.handleChange} />
-                        <label htmlFor="photo4">photo4:</label>
-                        <input id="photo4" name="photo4" type="text" placeholder="Ex: photoUrl" onChange={this.handleChange} />
-                        <button type="submit">Sign Up Model!</button>
+                        <Input>
+                            <label htmlFor="name">Name:</label>
+                            <input id="name" name="name" type="text" placeholder="Ex: Jesus Santos" onChange={this.handleChange} />
+                            <label htmlFor="dob">DOB:</label>
+                            <input id="dob" name="dob" placeholder="Ex: 7/12/2010" type="text" onChange={this.handleChange} />
+                            <label htmlFor="ethnicity">Ethnicity:</label>
+                            <input id="ethnicity" name="ethnicity" type="text" placeholder="Ex: White" onChange={this.handleChange} />
+                            <label htmlFor="location">Location:</label>
+                            <input id="location" name="location" type="text" placeholder="Ex: Houston, Tx" onChange={this.handleChange} />
+                        </Input>
+                        <Photos>
+                            <label htmlFor="photo1">Photo 1:</label>
+                            <input id="photo1" name="photo1" type="text" placeholder="Ex: photoUrl" onChange={this.handleChange} />
+                            <label htmlFor="photo2">Photo 2:</label>
+                            <input id="photo2" name="photo2" type="text" placeholder="Ex: photoUrl" onChange={this.handleChange} />
+                            <label htmlFor="photo3">Photo 3:</label>
+                            <input id="photo3" name="photo3" type="text" placeholder="Ex: photoUrl" onChange={this.handleChange} />
+                            <label htmlFor="photo4">Photo 4:</label>
+                            <input id="photo4" name="photo4" type="text" placeholder="Ex: photoUrl" onChange={this.handleChange} />
+                        </Photos>
+                        <Button type="submit">Sign Up Model!</Button>
                     </form>
                 </body>
             </div>
