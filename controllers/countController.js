9ba889aea35679d1a7d32c0398ffd521ddcff3da
JoseTyler101 <JoseTyler101@gmail.com>
2019-06-17 12:05:56 -0400

    added my site to Counter API

--- controllers/countController.js
+++ controllers/countController.js
@@ -1,12 +1,10 @@
 // import axios from "axios"
 var axios = require('axios');
 
-
-
 const countController = {
     index: function (req, res) {
         console.log("Hello")
-        axios.get('https://api.countapi.xyz/hit/countapi.xyz/visits?').then((response) => {
+        axios.get('https://api.countapi.xyz/hit/guarded-lake-14667.herokuapp.com/visits?').then((response) => {
             console.log(response.data)
            
             res.send(response.data)
